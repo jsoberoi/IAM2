a03b793b9646f3e066b3e7393eb9c39873c59e9b
jsoberoi <jsoberoi@users.noreply.github.com>
2019-05-27 16:43:57 +1200

    Create index.php

---
+++ index.php
@@ -0,0 +1,252 @@
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Best in NZ & AU security consulting and managed security services provider, with an exceptional customer satisfaction approach.">
+    <meta name="keywords" content="security, compliance, assessment, consulting, managed services">
+
+    <title>SeCompAss - Your compass in security.</title>
+
+    <!-- Styles -->
+    <link href="assets/css/core.min.css" rel="stylesheet">
+    <link href="assets/css/thesaas.min.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="timeline/css/style.css" rel="stylesheet">
+
+
+    <!-- Favicons -->
+<!--    <link rel="apple-touch-icon" href="assets/img/apple-touch-icon.png"> -->
+<!--<link rel="icon" href="assets/img/favicon.png">
+-->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-114339740-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-114339740-1');
+gtag('config', 'AW-971544636');
+</script>
+
+    <script src="assets/code/highcharts.js"></script>
+
+  </head>
+
+  <body>
+
+    <nav class="topbar topbar-inverse topbar-expand-md topbar-sticky">
+      <div class="container">
+        
+        <div class="topbar-left">
+          <button class="topbar-toggler">&#9776;</button>
+          <a class="topbar-brand" href="index.php">
+            <img class="logo-default" src="assets/img/secompass.png" alt="logo">
+            <img class="logo-inverse" src="assets/img/secompass-light.png" alt="logo">
+          </a>
+        </div>
+
+
+        <div class="topbar-right">
+          <ul class="topbar-nav nav">
+            <li class="nav-item"><a class="nav-link active" href="index.php">Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="about_us.php">About us</a></li>
+         <li class="nav-item">
+              <a class="nav-link " href="#">Assessment <i class="fa fa-caret-down"></i></a>
+              <div class="nav-submenu">
+ 	       <a class="nav-link" href="agile_risk_assessment.php">Security Risk Assessment</a>
+	         <a class="nav-link" href="architecture_review.php">Architectural Security Review</a>
+               <a class="nav-link" href="vulnerability_assessment.php">Vulnerability Assessment</a>
+                <a class="nav-link" href="gap_assessment.php">Security Gap Assessment</a>
+  
+              </div>
+	    </li> 
+             <li class="nav-item"><a class="nav-link " href="implementation.php">Implementation</a></li>
+           <li class="nav-item"><a class="nav-link " href="assurance.php">Assurance</a></li>
+<!--           <li class="nav-item"><a class="nav-link " href="soc.php">SOCaaS</a></li>
+-->
+
+
+
+            <li class="nav-item">
+              <a class="nav-link " href="#">Blog <i class="fa fa-caret-down"></i></a>
+              <div class="nav-submenu">
+	         <a class="nav-link" href="agile.php">Agile&Security</a>
+    <!--           <a class="nav-link" href="gdpr.php">GDPR</a>
+  -->
+              </div>
+	    </li> 
+ 
+            <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
+
+
+
+         </ul>
+        </div>
+
+      </div>
+    </nav>
+ 
+
+
+
+    <!-- Header -->
+   <!-- <header class="header fadeout header-inverse pb-0 h-fullscreen" style="background-image: linear-gradient(to bottom, #243949 0%, #517fa4 100%);">
+   --> <header class="header header-inverse h-fullscreen pb-80" style="background-image: url(assets/img/bg3.jpg);" data-overlay="8">
+
+<!--      <canvas class="constellation"></canvas>
+ -->
+      <div class="container">
+        <div class="row h-full">
+          <div class="col-12 text-center align-self-center">
+            <h1 class="fs-50 fw-600 lh-15 hidden-sm-down">We do what we promise and <br> we only promise what we can do<br> </h1>
+            <h1 class="fs-35 fw-600 lh-15 hidden-md-up">We do what we promise and <br> we only promise what we can do<br> </h1>
+            <br>
+            <p class="fs-20 hidden-sm-down"><strong>Sit back, relax, and watch how we deliver security for you.</strong></p>
+            <p class="fs-16 hidden-md-up"><strong>Sit back, relax, and watch how we deliver security for you.</strong></p>
+           <br>
+            <hr class="w-60 hidden-sm-down">
+            <br>
+
+
+            <a class="btn btn-lg btn-round w-250 btn-outline btn-white hidden-sm-down" href="#" data-scrollto="how-it-works">Why us?</a>
+
+            <a class="btn btn-lg btn-round w-250 btn-primary" href="contact.php" data-scrollto="free_consultation">Free Consultation</a>
+
+
+          <div class="col-12 align-self-end text-center pb-70">
+            <a class="scroll-down-2 scroll-down-inverse" href="#" data-scrollto="how-it-works"><span></span></a>
+
+          </div>
+        </div>
+      </div>
+    </header>
+    <!-- END Header -->
+
+  <section class="section py-40" id="how-it-works">
+        <div class="container">
+ 	   <header class="section-header">
+		<h2>Why Us?</h2>
+		<p>We give you the best tools and services to maximize your level of security, <br>without the stress of figuring it out yourself. </p>
+	  </header>
+   <div class="row gap-y">
+
+            <div class="col-lg-4">
+              <div class="card card-body border text-center">
+                <p class="my-5"><img src="assets/img/assessment.jpg" style="width:200px; height:200px;"/></p>
+                <h5>Assessment</h5>
+                <p>SeCompAss can help you identify and remediate security gaps in your enterprise architecture, technical security and business risk </p>
+              </div>
+            </div>
+
+
+            <div class="col-lg-4">
+              <div class="card card-body border text-center">
+                <p class="my-5"><img src="assets/img/cost_reduction.jpg" style="width:200px; height:200px;"/></p>
+                <h5>Implementation </h5>
+                <p>SeCompAss supports customers by owning and implementing complete security programs or specific security technologies like SIEM, IdM or DLP solutions.  </p>
+              </div>
+            </div>
+
+
+            <div class="col-lg-4">
+              <div class="card card-body border text-center">
+                <p class="my-5"><img src="assets/img/assurance.jpg" style="width:200px; height:200px;"/></p>
+                <h5>Assurance</h5>
+                <p>SeCompAss not only helps you improve your security posture, but also helps you obtain appropriate certifications, like ISO27001 , PCI DSS , GDPR or AICPA SOC1 and SOC2, that provide acknowledgement of the achieved your security maturity</p>
+              </div>
+            </div>
+
+          </div>
+	</div>
+  </section>
+
+
+  <section class="section section-inverse py-120" style="background-image: linear-gradient(120deg, #4eb0ed 0%, #167cbc 100%);" id="free_consultation">
+        <div class="container">
+          <header class="section-header">
+            <h2 class="fs-45">Free consultation, on any security related topic.  </h2>
+            <hr>
+Limited budget and conflicting priorities? <br>Let us help you to decide what will bring the most effective security spend in your organization.
+<br>
+
+          </header>
+
+          <p class="text-center">
+            <a class="btn btn-xl btn-primary w-350" href="contact.php"><span class="pl-4">Contact us today!</span></a><br>
+          </p>
+        </div>
+      </section>
+
+
+
+
+
+
+    </main>
+    <!-- END Main container -->
+
+
+
+
+
+    <!-- Footer -->
+    <footer class="site-footer">
+      <div class="container">
+        <div class="row gap-y align-items-center">
+          <div class="col-12 col-lg-3">
+            <p class="text-center text-lg-left">
+              <a href="index.php"><img src="assets/img/secompass.png" alt="logo"></a>
+            </p>
+          </div>
+
+          <div class="col-12 col-lg-6">
+            <ul class="nav nav-primary nav-hero">
+              <li class="nav-item hidden-sm-down">
+                <a class="nav-link" href="privacy.php">Privacy Policy</a>
+              </li>
+<!--	      <li class="nav-item hidden-sm-down">
+                <a class="nav-link" href="tnc.html">Terms and Conditions</a>
+              </li>
+-->
+
+
+            </ul>
+          </div>
+
+          <div class="col-12 col-lg-3">
+            <div class="social text-center text-lg-right">
+ <!--             <a class="social-facebook" href="https://www.facebook.com/secompass/"><i class="fa fa-facebook"></i></a> 
+     -->         <!-- <a class="social-twitter" href="https://twitter.com/secompass"><i class="fa fa-twitter"></i></a> --> 
+            </div>
+          </div>
+            </div>
+ <div>
+<hr>
+<small class="text-uppercase">
+By using the website and/or the service, you agree to the terms and conditions. If you do not agree, do not use the site and/or the service. Secompass reserves the right, at its sole discretion, to amend, change, modify, add or remove portions of these terms of service as published on secompass.com, at any time. Your continued use of the website and/or the service following the published updates to the terms will mean that you accept and agree to the changes.
+</small>
+</div>
+
+
+
+      </div>
+    </footer>
+    <!-- END Footer -->
+
+
+
+
+
+
+    <!-- Scripts -->
+    <script src="assets/js/core.min.js"></script>
+    <script src="assets/js/thesaas.min.js"></script>
+    <script src="assets/js/script.js"></script>
+
+  </body>
+</html>
